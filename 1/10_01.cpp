880689914319a4d362dc002c7caaf0b05a905eea
Eugene Halushkin <halushkib@yandex.ru>
2023-10-10 14:38:02 +0300

    Добавьте файлы проекта.

---
+++ 1/10_01.cpp
@@ -0,0 +1,19 @@
+﻿// 10_01.cpp : Задача 1. Простейший проект
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian"); //Корректное отображение Кириллицы
+	system("chcp 1251");
+	std::cout << std::endl;
+
+	std::string name;
+
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+
+	std::cout << "Здравствуйте, " << name << "!";
+	std::cout << std::endl;
+}
